0dab3e1036634ae3080b719352cbe93f39cd0306
scrips.weave <scrips.weave_0f@icloud.com>
2025-07-17 10:37:50 +0200

    Revert to previous working version

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { getDatabase, ref, set, onValue, push, get } from 'firebase/database';
 
 // REPLACE THIS WITH YOUR FIREBASE CONFIG FROM STEP 1.3
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY_HERE",
-  authDomain: "YOUR_AUTH_DOMAIN_HERE",
-  databaseURL: "YOUR_DATABASE_URL_HERE",
-  projectId: "YOUR_PROJECT_ID_HERE",
-  storageBucket: "YOUR_STORAGE_BUCKET_HERE",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID_HERE",
-  appId: "YOUR_APP_ID_HERE"
+  apiKey: "AIzaSyAgVVmCiipVGZdT-KxKSBFUpjBO3x9Th1s",
+  authDomain: "shark-tank-voting.firebaseapp.com",
+  databaseURL: "https://shark-tank-voting-default-rtdb.firebaseio.com",
+  projectId: "shark-tank-voting",
+  storageBucket: "shark-tank-voting.firebasestorage.app",
+  messagingSenderId: "368502305847",
+  appId: "1:368502305847:web:47e3aff46733312ed6fbb2"
 };
 
 const app = initializeApp(firebaseConfig);
